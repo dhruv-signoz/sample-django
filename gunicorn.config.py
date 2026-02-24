983887ef4e12c150dca08c891977bfedc97f336d
Dhruv Ahuja <dhruvahuja2k@gmail.com>
2026-02-24 10:41:24 +0530

    chore: re-establish gunicorn post_fork config

--- gunicorn.config.py
+++ gunicorn.config.py
@@ -4,19 +4,34 @@ from opentelemetry import trace
 from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor, ConsoleSpanExporter, SimpleSpanProcessor
 
-# from otel_config import init_otel
 
 
 def post_fork(server, worker):
     server.log.info("Worker spawned (pid: %s)", worker.pid)
 
+    resource = Resource.create(attributes={
+        "service.name": "api-service"
+    })
+
+    trace.set_tracer_provider(TracerProvider(resource=resource))
+    span_processor = BatchSpanProcessor(
+        OTLPSpanExporter(
+            endpoint="https://ingest.in.signoz.cloud:443",
+            headers={
+                "signoz-ingestion-key": "<your-ingestion-key-here>"
+            }
+        )
+    )
+
+    # export to both console and otlp for testing
+    trace.get_tracer_provider().add_span_processor(span_processor)
+    trace.get_tracer_provider().add_span_processor(SimpleSpanProcessor(ConsoleSpanExporter()))
 
     print("initializing opentelemtry SDK in the gunicorn.config.py")
-    init_otel()
 
-
+    # create span to see if the configuration works or still emits unauthenticated transient error!
     tracer = trace.get_tracer("app")
 
     with tracer.start_as_current_span("manual-from-gunicorn") as span:
